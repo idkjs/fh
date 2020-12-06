49de12f8aaf96a7ccd537af370a7af719a61340b
Alain Armand <aarmand.inbox@gmail.com>
2020-12-06 01:15:51 +0100

    port to fish:WIP

--- README.md
+++ README.md
@@ -2,6 +2,22 @@
 
 [`fh`](https://github.com/peterpme/dotfiles/blob/master/bin/fh) wrapper for fish using [bass](https://github.com/edc/bass).
 
+## ManPage
+
+```bash
+fc
+
+Open the most recent command and edit it.
+
+- Open in the default system editor:
+    fc
+
+- Specify an editor to open with:
+    fc -e 'emacs'
+
+- List recent commands from history:
+    fc -l
+```
 ## Installation
 
 Using [fisher](https://github.com/jorgebucaran/fisher)(recommended)
--- functions/fh.fish
+++ functions/fh.fish
@@ -1,4 +1,20 @@
 #!/usr/bin/env fish
+
+
+# function fh
+# # http://zsh.sourceforge.net/Doc/Release/Parameters.html#index-ZSH_005fNAME
+#     if [ -n $ZSH_NAME ]
+#         set FIRST (fc -l 1)
+#         print -z ( $FIRST | fzf +s --tac | sed 's/ *[0-9]* *//')
+#     else
+#         set FIRST ($history)
+#     end
+#     print -z ( $FIRST | fzf +s --tac | sed 's/ *[0-9]* *//')
+# end
+# cheating for now
 set script_dir (realpath (dirname (status --current-filename)))
 
-bass source $script_dir/fh
+function fh
+    # http://zsh.sourceforge.net/Doc/Release/Parameters.html#index-ZSH_005fNAME
+    bass $script_dir/fh.sh
+end
